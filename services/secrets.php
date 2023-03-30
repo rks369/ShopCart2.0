bf50db05ca2fc603c28a5ed44561233bbc5242cb
Ritesh <riteshsaini331@gmail.com>
2023-03-30 14:39:13 +0530

    Payment Gateway Demo Added

---
+++ services/secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51MrDCkSDYkK1dKTssgXfq7sLmJZdLU9hn0fnR7HOhUfJBO4c6poqFTTiUusd43KjiFeBu6o0IRAXv3QSiMgkMx7j00Ael0a441';
